b094ac1e4df3df4ba6ba1ece90c50f2a79e06134
Leroy6376 <roza4466@mail.ru>
2024-08-06 22:37:05 +0300

    Initial layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('content')
+{{--    @include('includes.shopCollections')--}}
+
+    @include('includes.newProducts')
+
+    @include('includes.hotDealSection')
+
+    @include('includes.topSelling')
+
+{{--    @include('includes.miniTopSelling')--}}
+
+    @include('includes.newsletter')
+@endsection
